d3aa5f89d7415c0337b62ef5629afc911681e61b
FrancisNgugi05 <francis.ngugi@student.moringaschool.com>
2024-02-07 18:00:51 +0300

    Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
-// create your App component here
+import React, {useState, useEffect} from "react";
+import { data } from "../mocks/data";
+
+function App() {
+    // The useState will be used to set the image
+    const [image, setImage] = useState(null);
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+            // coverting the API response to json format
+            .then(response => response.json())
+            // The image fetched will be set in the setImage funciton
+            .then((data) => setImage(data.message))
+    }, []);
+
+    // show Loading text if imgae is not there
+    if (image === null)
+        return (<p>Loading ...</p>);
+
+    return (
+        <div>
+            <img src={image} alt="dog image" />
+        </div>
+    )
+
+}
+
+export default App;
